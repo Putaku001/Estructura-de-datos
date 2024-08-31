1cc759703df4bd32bd91e4c6eff69fc64c678f03
Putaku001 <133188152+Putaku001@users.noreply.github.com>
2024-08-30 19:19:40 -0600

    Add files via upload

---
+++ practicas/p2/EjercicioDos.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val notas = DoubleArray(4)
+    var suma = 0.0
+
+    for (i in notas.indices) {
+        while(true){
+            try{
+                println("Ingrese la nota ${i +1}:")
+                notas[i] = readLine()!!.toDouble()
+                break
+            } catch (e: NumberFormatException){
+                println("Por favor ingrese una nota valida")
+            }
+        }
+
+        suma += notas[i]
+    }
+
+    val promedio = suma / notas.size
+    println("El promedio de las notas es de: $promedio")
+}
